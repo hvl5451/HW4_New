b30bc2a85dd5bcedd4dbcb1970ff3787d4044bf8
hvl5451 <32529065+hvl5451@users.noreply.github.com>
2018-03-14 00:35:01 -0400

    Update teller.py

--- teller.py
+++ teller.py
@@ -1,8 +1,8 @@
 from employee import Employee
 
 class Teller(Employee):
-    def __init__(self, name, birthdate, phone_no, password):
-        super().__init__(name, birthdate, phone_no,password)
+    def __init__(self, name, birthdate, phone_no,):
+        super().__init__(name, birthdate, phone_no)
 
 
 
