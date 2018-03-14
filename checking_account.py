214966e8b5aa265f89d649362e40f48d39d53d49
Isaac T <isaac.thomas1060@gmail.com>
2018-03-14 00:32:12 -0400

    checking account mods

--- checking_account.py
+++ checking_account.py
@@ -3,7 +3,7 @@ from account import Account
 class CheckingAccount(Account):
     def __init__(self, holder, created_on, account_number, account_type, balance=0.0 ):
         super().__init__(holder, created_on, account_number, account_type, balance)
-        
+
     def charge_interest(self):
         self._balance+=(CheckingAccount.interest*self._balance)
         Account.update_balance(self.balance)
