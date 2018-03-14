7fc3c2878b49f4e5b45b7d7e55b59e93a92ace39
hvl5451 <32529065+hvl5451@users.noreply.github.com>
2018-03-14 00:52:35 -0400

    Update teller.py

--- teller.py
+++ teller.py
@@ -1,9 +1,12 @@
+"""
+Teller inherits from Employee
+The important function of the teller would display the customer details which is obtained from employee class
+"""
+
 from employee import Employee
 
 class Teller(Employee):
-    def __init__(self, name, birthdate, phone_no,):
-        super().__init__(name, birthdate, phone_no)
-
-
+    def __init__(self, name, birthdate, phone_no, password):
+        super().__init__(name, birthdate, phone_no,password)
 
 
