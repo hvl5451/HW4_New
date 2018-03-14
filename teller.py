bd0d84ef68a464562ed8647ecdb653d2a28f6e97
Isaac T <isaac.thomas1060@gmail.com>
2018-03-14 01:19:00 -0400

    biggest docs commit ever

--- teller.py
+++ teller.py
@@ -6,7 +6,5 @@ The important function of the teller would display the customer details which is
 from employee import Employee
 
 class Teller(Employee):
-    def __init__(self, name, birthdate, phone_no, password):
-        super().__init__(name, birthdate, phone_no,password)
-
-
+    def __init__(self, name, birthdate, eid, phone_no):
+        super().__init__(name, birthdate, eid, phone_no)
