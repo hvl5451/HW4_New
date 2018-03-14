7260f2f62d841b3063f100e3d936b04924bd4c5f
matthewli125 <37080094+matthewli125@users.noreply.github.com>
2018-03-13 23:18:12 -0400

    Update customer.py

--- customer.py
+++ customer.py
@@ -8,7 +8,8 @@ deposit, withdraw, and check balance.
 
 
 from person import Person
-from account import Account
+from checking_account import CheckingAccount
+from savings_account import SavingsAccount
 import threading
 import random
 import datetime
@@ -27,7 +28,8 @@ class Customer(Person):
     
     def __init__(self, name, birthdate, phone_number, \
                  password = None, customer_number = None):
-        self.__password = password
+        
+        self.___password = password
         self.__customer_account_list = {}
         
         super().__init__(name, birthdate, phone_number)
@@ -42,51 +44,70 @@ class Customer(Person):
 
 
     def create_usraccount(self, password, birthdate):
-        if birthdate != self.birthdate:
+        
+        if birthdate != self._birthdate:
             raise ValueError("birthdates do not match. Security verification failed")
+        
         else:
-            self.__password = password
-            self.customer_number = random.randint(1000000000,10000000000)
-            print("account number is {}".format(self.customer_number))
+            self.___password = password
+            self._customer_number = random.randint(1000000000,10000000000)
+            print("account number is {}".format(self._customer_number))
 
 
             
         
     def set_password(self, oldpass, newpass):
-        self.__password = newpass
+        
+        self._password = newpass
 
+
+    def get_name(self):
+        return self._name
+
+    def get_phone_number(self):
+        return self._phone
 
     def set_name(self, temppass, newname):
-        self.name = newname
+        
+        self._name = newname
 
 
     def set_phone_number(self, temppass, new_phone_number):
-        self.phone_number = new_phone_number
+        self._phone_number = new_phone_number
 
 
     def create_account(self, account_type):
-        new_account = Account (self.name, str(datetime.datetime.now()), random.randint(1000000,10000000), account_type)
-        self.__customer_account_list[new_account.get_account_number()] = new_account
-        self.__customer_account_list[new_account.get_account_number()] = new_account
+        
+        if account_type.lower() == "checking":
+            new_account = CheckingAccount (self.___name, str(datetime.datetime.now()), random.randint(1000000,10000000), account_type)
+            
+        if account_type.lower() == "saving":
+            new_account = SavingsAccount (self.___name, str(datetime.datetime.now()), random.randint(1000000,10000000), account_type)
+
+            
+        self._customer_account_list[new_account.get_account_number()] = new_account
+        self._customer_account_list[new_account.get_account_number()] = new_account
+
+        
         print("account number is {}".format(new_account.get_account_number()))
         return new_account.get_account_number()
             
 
     def check_balance(self, account_number):
-        return self.__customer_account_list[account_number].get_balance()
+        return self._customer_account_list[account_number].get_balance()
 
     def withdraw(self, account_number, amount):
-        self.__customer_account_list[account_number].update_balance(amount*-1)
+        self._customer_account_list[account_number].update_balance(amount*-1)
         print(" withdrew ${}", amount)
 
 
     def deposit(self, account_number, amount):
-        self.__customer_account_list[account_number].update_balance(amount)
+        self._customer_account_list[account_number].update_balance(amount)
         print(" withdrew ${}", amount)        
         
         
     def __str__(self):
-        return "{},{}".format(self.name, self.__customer_number)
+        return "{},{}".format(self._name, self._customer_number)
 
     __repr__=__str__
     
