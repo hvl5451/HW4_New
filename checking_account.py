b6127a8975fde2813dd7903d013f9af4105cfea5
hvl5451 <32529065+hvl5451@users.noreply.github.com>
2018-03-13 22:06:08 -0400

    Update checking_account.py

--- checking_account.py
+++ checking_account.py
@@ -5,5 +5,5 @@ class CheckingAccount(Account):
         super().__init__(holder, created_on, account_number, account_type, balance)
         
     def charge_interest(self):
-        self._balance+=(SavingsAccount.interest*self._balance)
+        self._balance+=(CheckingAccount.interest*self._balance)
         Account.update_balance(self.balance)
